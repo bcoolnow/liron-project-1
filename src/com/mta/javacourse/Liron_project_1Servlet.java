71b5aa915d009551e3b71ea3479c52b9f98c5eae
bcoolnow <bcoolnow@gmail.com>
2014-11-09 07:22:04 +0200

    Result of ( 4+3) *7=49

--- src/com/mta/javacourse/Liron_project_1Servlet.java
+++ src/com/mta/javacourse/Liron_project_1Servlet.java
@@ -1,11 +1,20 @@
 package com.mta.javacourse;
 import java.io.IOException;
 import javax.servlet.http.*;
-
 @SuppressWarnings("serial")
+
 public class Liron_project_1Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, liron");
+		int num1, num2, num3;
+		num1=4;
+		num2=3;
+		num3=7;
+		int result=(num1+num2)*num3;
+		String resultStr = new String("<h1>Result of ( "+num1+"+"+num2+") *"+num3+"="+result+"</h1>");
+		resp.setContentType("text/html");
+		resp.getWriter().println(resultStr);
+
 	}
+	
 }
+
