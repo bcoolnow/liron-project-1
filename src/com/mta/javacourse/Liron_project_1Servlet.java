d7df28945008882c1f789d22ae5a852564126c6d
bcoolnow <bcoolnow@gmail.com>
2014-11-15 12:45:08 +0200

    Exercise 3

--- src/com/mta/javacourse/Liron_project_1Servlet.java
+++ src/com/mta/javacourse/Liron_project_1Servlet.java
@@ -5,14 +5,25 @@ import javax.servlet.http.*;
 
 public class Liron_project_1Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		int num1, num2, num3;
+		int num1, num2, num3,radius;
 		num1=4;
 		num2=3;
 		num3=7;
+		radius=50;
 		int result=(num1+num2)*num3;
+		double area=Math.PI*(radius*radius);
+		double resultofpower=Math.pow(20,13);
+
+		String line1 = new String("calculation 1: Area of circle with radius ("+radius+") is ("+area+") square cm ");
+		String line2 = new String("calculation 2: Length of opposite	where angle B ...");
+		String line3 = new String("calculation 3: Power of 20 with exp 	of 13 is ("+resultofpower+")");
 		String resultStr = new String("<h1>Result of ( "+num1+"+"+num2+") *"+num3+"="+result+"</h1>");
+		String resultStr2 = line1 + "<br>" + line2 + "<br>" +line3;
 		resp.setContentType("text/html");
+    	resp.getWriter().println("Exercise 2 :<br>");
 		resp.getWriter().println(resultStr);
+    	resp.getWriter().println("Exercise 3 :<br><br>");
+		resp.getWriter().println(resultStr2);
 
 	}
 	
